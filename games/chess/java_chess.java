7bc015163d383fa95c7d9fe354af2ca34d53e350
YuunoDev <adrianalonso.a4@proton.me>
2026-09-17 17:28:34 -0600

    Enums y clase pieces

--- games/chess/java_chess.java
+++ games/chess/java_chess.java
@@ -3,7 +3,7 @@ package games.chess;
 public class java_chess {
     public static void main(String[] args) {
         
-        int[][] myNumbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+        
 
 
     }
--- games/chess/piece.java
+++ games/chess/piece.java
@@ -1,5 +1,69 @@
 package games.chess;
 
+//posición       ("e",1)
+record Position(String letra, int numero) {
+}
+
+enum Color {
+    B,
+    W
+}
+
 public class piece {
-    public String name;
+    private String name;
+    private Position position;
+    private Color color;
+    private Boolean first;
+    private Boolean isActive;
+
+    public piece(String name, Position position, Color color)
+    {
+        this.name = name;
+        this.position = position;
+        this.color = color;
+        this.first = false;
+        this.isActive = true;
+    }
+
+    public piece(String name, Position position, Color color, Boolean first) {
+        this.name = name;
+        this.position = position;
+        this.color = color;
+        this.first = first;
+    }
+
+    public Boolean getFirst() {
+        return first;
+    }
+
+    public void setFirst(Boolean first) {
+        this.first = first;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    
+
 }
